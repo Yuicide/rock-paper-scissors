6472d470b2d14f753797ea9cddd0091815fe7f08
Yuicide <rid3rsrb@gmail.com>
2022-11-17 21:29:57 +0100

    Add function to get Player choice
    Function getPlayerChoice makes prompt asking
    for either Rock, Paper or Scissors.
    
    After user type the choice, the function automatically
    converts letters to lower case so it can match with
    elements from global array.
    
    There is a while loop so that if the user press cancel
    on the prompt, it will just loop until something is
    typed or the "ok" button is pressed.

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
-    console.log(choices[Math.floor(Math.random() * choices.length)]);
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function getPlayerChoice(){
+    let input = prompt("Do you choose Rock, Paper or Scissors?");
+    while(input == null){
+        input = prompt("Do you choose Rock ,Paper or Scissors?");
+    }
+    input = input.toLowerCase();
+    console.log(input);
 }
 
 getComputerChoice();
+getPlayerChoice();
